d1996e4c041cb16b6787498adb9fecfcae153cdd
Thalida Noel <thalida.c.noel@gmail.com>
2022-11-30 02:55:36 +0000

    Setup Vue App

---
+++ app/src/App.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+import { RouterView } from "vue-router";
+</script>
+
+<template>
+  <header>Sudoku</header>
+  <RouterView />
+</template>
---
+++ app/src/views/HomeView.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import GameBoard from "@/components/GameBoard.vue";
+</script>
+
+<template>
+  <main>
+    <GameBoard />
+  </main>
+</template>
